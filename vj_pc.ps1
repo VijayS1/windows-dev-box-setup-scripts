216fcfefa2b14c6c882d1f235ce8d55d99a572c6
VijayS1 <vijay+code@saraff.com>
2019-12-11 00:28:14 +0700

    visual studio added

--- vj_pc.ps1
+++ vj_pc.ps1
@@ -1,6 +1,8 @@
 # Description: Boxstarter Script
-# Author: Microsoft
-# Common settings for web dev
+# Author: Vijay
+# how to run? Visit the url below in a browser
+# it will launch a one-click application
+# http://boxstarter.org/package/url?https://raw.githubusercontent.com/VijayS1/windows-dev-box-setup-scripts/master/vj_pc.ps1
 
 Disable-UAC
 
@@ -18,7 +20,7 @@ write-host "helper script base URI is $helperUri"
 function executeScript {
     Param ([string]$script)
     write-host "executing $helperUri/$script ..."
-	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
+	Invoke-Expression ((new-object net.webclient).DownloadString("$helperUri/$script"))
 }
 
 #--- Setting up Windows ---
@@ -33,6 +35,8 @@ executeScript "Browsers.ps1";
 executeScript "VJApps.ps1";
 
 #--- Tools ---
+choco install -y visualstudio2019community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
+#this will work only if code is in path
 code --install-extension nonoroazoro.syncing #To sync VSCode
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension msjsdiag.debugger-for-firefox
