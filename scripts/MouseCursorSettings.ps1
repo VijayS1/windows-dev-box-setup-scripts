bff96121c4a9b5919633bdc669b601e707c0afe2
VijayS1 <vijay+code@saraff.com>
2020-01-08 00:34:56 +0700

    Added new method of updating registry

--- scripts/MouseCursorSettings.ps1
+++ scripts/MouseCursorSettings.ps1
@@ -2,34 +2,58 @@
 $regpath = "HKCU:\Control Panel\Cursors"
 $cursorpath = "%LocalAppData%\Microsoft\Windows\Cursors\"
 
-$settings = @(@{Path=$regpath;Name="(Default)";Value="Windows Inverted";PropertyType="String";Force=$true},
-              @{Path=$regpath;Name="CursorBaseSize";Value=64;PropertyType="DWord";Force=$true}
+$suffix = "_eoa.cur"
+$settings = @(@{Path="."; Force=$true; PropertyType="String";       Name="(Default)";     Value="Windows Inverted"}
+              @{Path="."; Force=$true; PropertyType="DWord";        Name=CursorBaseSize;  Value=64}
+              @{Path="."; Force=$true; PropertyType="DWord";        Name="Scheme Source"; Value=2}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=Appstarting;     Value=$cursorpath + "busy"   +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=Arrow;           Value=$cursorpath + "arrow"  +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=Crosshair;       Value=$cursorpath + "cross"  +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=Hand;            Value=$cursorpath + "link"   +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=Help;            Value=$cursorpath + "helpsel"+$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=IBeam;           Value=$cursorpath + "ibeam"  +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=No;              Value=$cursorpath + "no"     +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=NWPen;           Value=$cursorpath + "pen"    +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=Person;          Value=$cursorpath + "person" +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=Pin;             Value=$cursorpath + "pin"    +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=SizeAll;         Value=$cursorpath + "move"   +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=SizeNESW;        Value=$cursorpath + "nesw"   +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=SizeNS;          Value=$cursorpath + "ns"     +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=SizeNWSE;        Value=$cursorpath + "nwse"   +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=SizeWE;          Value=$cursorpath + "ew"     +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=UpArrow;         Value=$cursorpath + "up"     +$suffix}
+              @{Path="."; Force=$true; PropertyType="ExpandString"; Name=Wait;            Value=$cursorpath + "wait"   +$suffix}
              )
-             
-#set location to registry folder where we will make all changes
-#Set-Location $regpath
-#Set Mouse Pointer Scheme to Windows Inverted, large size
 
-New-ItemProperty -Path $regpath -Name "(Default)"      -Value "Windows Inverted"                 -PropertyType String       -Force
-New-ItemProperty -Path $regpath -Name "CursorBaseSize" -Value 64                                 -PropertyType DWord        -Force
-New-ItemProperty -Path $regpath -Name "Scheme Source"  -Value 2                                  -PropertyType DWord        -Force
-New-ItemProperty -Path $regpath -Name AppStarting      -Value ($cursorpath + "busy_eoa.cur")     -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name Arrow            -Value ($cursorpath + "arrow_eoa.cur")    -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name Crosshair        -Value ($cursorpath + "cross_eoa.cur")    -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name Hand             -Value ($cursorpath + "link_eoa.cur")     -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name Help             -Value ($cursorpath + "helpsel_eoa.cur")  -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name IBeam            -Value ($cursorpath + "ibeam_eoa.cur")    -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name No               -Value ($cursorpath + "no_eoa.cur")       -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name NWPen            -Value ($cursorpath + "pen_eoa.cur")      -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name Person           -Value ($cursorpath + "person_eoa.cur")   -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name Pin              -Value ($cursorpath + "pin_eoa.cur")      -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name SizeAll          -Value ($cursorpath + "move_eoa.cur")     -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name SizeNESW         -Value ($cursorpath + "nesw_eoa.cur")     -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name SizeNS           -Value ($cursorpath + "ns_eoa.cur")       -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name SizeNWSE         -Value ($cursorpath + "nwse_eoa.cur")     -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name SizeWE           -Value ($cursorpath + "ew_eoa.cur")       -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name UpArrow          -Value ($cursorpath + "up_eoa.cur")       -PropertyType ExpandString -Force
-New-ItemProperty -Path $regpath -Name Wait             -Value ($cursorpath + "wait_eoa.cur")     -PropertyType ExpandString -Force
+Set-Location $regpath #set location to registry folder where we will make all changes
+foreach ($regkey in $settings) {
+#    New-ItemProperty @regkey #create all the registry keys
+    Write-Debug "Setting: " + $regkey
+}
+#$settings.ForEach(New-ItemProperty @_) #alternative method testing
+
+
+#Set Mouse Pointer Scheme to Windows Inverted, large size
+New-ItemProperty -Path $regpath -Name "(Default)"      -Value "Windows Inverted"                    -PropertyType String       -Force
+New-ItemProperty -Path $regpath -Name CursorBaseSize   -Value 64                                    -PropertyType DWord        -Force
+New-ItemProperty -Path $regpath -Name "Scheme Source"  -Value 2                                     -PropertyType DWord        -Force
+New-ItemProperty -Path $regpath -Name AppStarting      -Value ($cursorpath + "busy"   + "_eoa.cur") -PropertyType ExpandString -Force
+New-ItemProperty -Path $regpath -Name Arrow            -Value ($cursorpath + "arrow"  + "_eoa.cur") -PropertyType ExpandString -Force #same
+New-ItemProperty -Path $regpath -Name Crosshair        -Value ($cursorpath + "cross"  + "_eoa.cur") -PropertyType ExpandString -Force
+New-ItemProperty -Path $regpath -Name Hand             -Value ($cursorpath + "link"   + "_eoa.cur") -PropertyType ExpandString -Force
+New-ItemProperty -Path $regpath -Name Help             -Value ($cursorpath + "helpsel"+ "_eoa.cur") -PropertyType ExpandString -Force
+New-ItemProperty -Path $regpath -Name IBeam            -Value ($cursorpath + "ibeam"  + "_eoa.cur") -PropertyType ExpandString -Force #same
+New-ItemProperty -Path $regpath -Name No               -Value ($cursorpath + "no"     + "_eoa.cur") -PropertyType ExpandString -Force #same
+New-ItemProperty -Path $regpath -Name NWPen            -Value ($cursorpath + "pen"    + "_eoa.cur") -PropertyType ExpandString -Force
+New-ItemProperty -Path $regpath -Name Person           -Value ($cursorpath + "person" + "_eoa.cur") -PropertyType ExpandString -Force #same
+New-ItemProperty -Path $regpath -Name Pin              -Value ($cursorpath + "pin"    + "_eoa.cur") -PropertyType ExpandString -Force #same
+New-ItemProperty -Path $regpath -Name SizeAll          -Value ($cursorpath + "move"   + "_eoa.cur") -PropertyType ExpandString -Force
+New-ItemProperty -Path $regpath -Name SizeNESW         -Value ($cursorpath + "nesw"   + "_eoa.cur") -PropertyType ExpandString -Force
+New-ItemProperty -Path $regpath -Name SizeNS           -Value ($cursorpath + "ns"     + "_eoa.cur") -PropertyType ExpandString -Force
+New-ItemProperty -Path $regpath -Name SizeNWSE         -Value ($cursorpath + "nwse"   + "_eoa.cur") -PropertyType ExpandString -Force
+New-ItemProperty -Path $regpath -Name SizeWE           -Value ($cursorpath + "ew"     + "_eoa.cur") -PropertyType ExpandString -Force
+New-ItemProperty -Path $regpath -Name UpArrow          -Value ($cursorpath + "up"     + "_eoa.cur") -PropertyType ExpandString -Force
+New-ItemProperty -Path $regpath -Name Wait             -Value ($cursorpath + "wait"   + "_eoa.cur") -PropertyType ExpandString -Force #same
  
 #Enable Pointer Option: Show location of pointer when I press the CTRL key
 # not sure about this, as we should actually be doing a bitwise OR operation from previous result to our desired result to get the actual mask
