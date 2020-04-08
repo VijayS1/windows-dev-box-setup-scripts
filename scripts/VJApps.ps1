84a95669d1587b367b21e5f6a11267f4df31f608
Vijay <vijay+code@saraff.com>
2020-04-09 02:06:44 +0700

    updated to support multiple lists for home/office ..etc.

--- scripts/VJApps.ps1
+++ scripts/VJApps.ps1
@@ -15,7 +15,18 @@
 
     #  $officeapplist = @(
     # )
-
+    $homeapplist = @(
+        "steam"
+        "discord"
+        "popcorn-time" #older version in chocolatey though as of 2020-04-05
+        "potplayer"
+        "ds4windows"
+        "forticlientvpn"
+   )
+    #optional "glasswire"
+    #optional "malwarebytes"
+    #zerotier-one
+    #radmin-vpn
     # $deprecatedlist = @(
     #     "nfopad"
     #     "notepad2"
@@ -24,31 +35,31 @@
     #     )
 
     $applicationList = @(
-    "directoryopus"
-    "notepadplusplus"
-    "ditto"
-    "listary"
-    "autohotkey"
-    "lastpass"
-    "google-hangouts-chat"
-    "office365ProPlus"
-    "google-backup-and-sync"
-    "resilio-sync-home"
-    "vlc"
-    "teamviewer12"
-    "vnc-connect"
-    "chocolateygui"
-    "manictime.install"
-    "forticlientvpn"
-    )
+        "directoryopus"
+        "notepadplusplus"
+        "ditto"
+        "listary"
+        "autohotkey"
+        "lastpass"
+        "google-hangouts-chat"
+        "office365ProPlus"
+        "google-backup-and-sync"
+        "resilio-sync-home"
+        "vlc"
+        "teamviewer12"
+        "vnc-connect"
+        "chocolateygui"
+        "manictime.install"
+     )
     function cinstApp {
-        Param ([string]$appName)
+        Param ([string[]]$appList = $(throw "Please pass the application(s) name you wish to install."))
         #write-debug "choco install -ys $appName" #DEBUG
         #write-debug "choco info -y $appName" #for debugging
-        choco install -y $appName
+        foreach ($appName in $appList) {
+            Write-Debug "choco info $($appName)"
+            #choco install -y $appName
+        }
     }
     
-    foreach ($app in $applicationList) {
-        cinstApp $app
-    }
-    
+    cinstApp($applicationList)
+    cinstApp($homeapplist)
