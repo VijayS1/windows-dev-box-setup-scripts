a8f8dfa57ccadeb558b2a4c1d9c4b4aed1d4e622
Vijay <vijay+code@saraff.com>
2020-04-09 15:41:38 +0700

    changed order of installation to prevent false reboots

--- vj_pc.ps1
+++ vj_pc.ps1
@@ -4,8 +4,6 @@
 # it will launch a one-click application
 # http://boxstarter.org/package/url?https://raw.githubusercontent.com/VijayS1/windows-dev-box-setup-scripts/master/vj_pc.ps1
 
-Disable-UAC
-
 # Get the base URI path from the ScriptToCall value
 $bstrappackage = "-bootstrapPackage"
 $helperUri = $Boxstarter['ScriptToCall']
@@ -24,19 +22,22 @@ function executeScript {
 }
 
 #--- Setting up Windows ---
-executeScript "FileExplorerSettings.ps1";
-executeScript "SystemConfiguration.ps1";
+Disable-UAC
+#apps
+executeScript "Browsers.ps1";
+executeScript "VJApps.ps1";
 executeScript "CommonDevTools.ps1";
-executeScript "RemoveDefaultApps.ps1";
 executeScript "HyperV.ps1";
 executeScript "Docker.ps1";
 executeScript "WSL.ps1";
-executeScript "Browsers.ps1";
-executeScript "VJApps.ps1";
+#settings
+executeScript "FileExplorerSettings.ps1";
+executeScript "SystemConfiguration.ps1";
+executeScript "RemoveDefaultApps.ps1";
 executeScript "MouseCursorSettings.ps1"; #moved below because it was causing problems with the other scripts I believe.
-executeScript "InstallStoreApps.ps1";
 executeScript "OEMInformationSettings.ps1";
-
+#manual step
+executeScript "InstallStoreApps.ps1";
 
 Enable-UAC
 Enable-MicrosoftUpdate
